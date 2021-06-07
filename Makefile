d8090bb5fd555f4ebb38112e8db78b1fa60c36cd
Remco <rpet@student.codam.nl>
2021-06-07 11:06:51 +0200

    Add files via upload

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: rpet <marvin@codam.nl>                       +#+                      #
 #                                                    +#+                       #
 #    Created: 2020/11/30 13:04:14 by rpet          #+#    #+#                  #
-#    Updated: 2021/05/27 13:29:02 by rpet          ########   odam.nl          #
+#    Updated: 2021/06/03 12:50:39 by rpet          ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
@@ -26,8 +26,8 @@ _CONTAINERS = list.hpp \
 _UTILS = ListNode.hpp \
 		 ListIterator.hpp \
 		 RandomAccessIterator.hpp \
-		 BidirectionalIterator.hpp \
-		 Iterator.hpp
+	#	 BidirectionalIterator.hpp \
+	#	 Iterator.hpp
 CONTAINERS = $(addprefix $(DIRCONTAINERS), $(_CONTAINERS))
 UTILS = $(addprefix $(DIRUTILS), $(_UTILS))
 INC = $(CONTAINERS) $(UTILS)
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: rpet <marvin@codam.nl>                       +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/05/24 07:26:12 by rpet          #+#    #+#                 */
-/*   Updated: 2021/05/27 09:27:30 by rpet          ########   odam.nl         */
+/*   Updated: 2021/06/07 10:55:50 by rpet          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,36 @@
 #include "stack.hpp"
 #include "queue.hpp"
 #include "ListNode.hpp"
+//#include "Iterator.hpp"
+//#include "BidirectionalIterator.hpp"
+#include "ListIterator.hpp"
 #include <iostream>
 #include <list>
 
 int		main()
 {
-	ft::list<int>			lst1(6, 13);
-	ft::list<int>::iterator	it = lst1.begin();
+	ft::list<int>				lst1(5, 10);
+	std::list<int>				real(5, 10);
 
+	lst1.push_front(16);
+	lst1.push_back(42);
 	lst1.debug();
-	std::cout << *it << std::endl;
+	real.push_front(16);
+	real.push_back(42);
+	
+	ft::list<int>::iterator		it = lst1.begin();
+	ft::list<int>::iterator		ite = lst1.end();
+	std::list<int>::iterator	realit = real.begin();
+	std::list<int>::iterator	realite = real.end();
+
+	std::cout << "lst1.begin(): " << *it << std::endl;
+	std::cout << "real.begin(): " << *realit << std::endl;
+	
+	ite--;
+	realite--;
+
+	std::cout << "lst1.end(): " << *ite << std::endl;
+	std::cout << "real.end(): " << *realite << std::endl;
 
 // Testing for leaks and calling destructor manually
 //	lst1.~list();
